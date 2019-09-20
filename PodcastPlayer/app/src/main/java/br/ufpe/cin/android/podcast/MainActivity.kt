83179bba54dff9d640a3c095219800149eca4d55
rllima <rlo@cin.ufpe.br>
2019-09-20 00:59:24 -0300

    2. done

--- PodcastPlayer/app/src/main/java/br/ufpe/cin/android/podcast/MainActivity.kt
+++ PodcastPlayer/app/src/main/java/br/ufpe/cin/android/podcast/MainActivity.kt
@@ -2,11 +2,18 @@ package br.ufpe.cin.android.podcast
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import org.jetbrains.anko.doAsync
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        doAsync {
+            val xml = URL("https://s3-us-west-1.amazonaws.com/podcasts.thepolyglotdeveloper.com/podcast.xml").readText()
+            val itemFeedList : List<ItemFeed> = Parser.parse(xml)
+        }
     }
 }
